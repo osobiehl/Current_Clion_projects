c3bb4d829a7ae6eccd94b4fe2f013cf76c703443
osobiehl <osobiehl@me.com>
2020-11-19 17:04:21 +0100

    finished os hw 10

---
+++ OS-HW-10/cmake-build-debug/CMakeFiles/OS_HW_10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_HW_10.dir/main.c.o"
+  "OS_HW_10"
+  "OS_HW_10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OS_HW_10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
